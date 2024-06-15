a0489cc6393c1b49e7e2134fb4f3cd3a692a45be
maximekjz <maximekajzer@gmail.com>
2024-06-15 22:51:34 +0300

    All done

---
+++ Week 2/Day3/D3/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+dict2 = {"Name": "Hen"}
+# dict3 = {"Name": "Ron"}
+
+# dictionaries = [dict1, dict2, dict3]
+
+# for dictionary in dictionaries:
+#     print(f"Hello {dictionary["Name"]}!")
+
+# 11:25
