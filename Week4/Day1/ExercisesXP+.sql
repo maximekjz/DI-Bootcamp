0c99f2774d684b2808f4beb1c0fb43401c6e2af9
maximekjz <maximekajzer@gmail.com>
2024-06-23 15:55:57 +0300

    XP done

---
+++ Week4/Day1/ExercisesXP+.sql
@@ -0,0 +1,28 @@
+CREATE TABLE students (
+  id serial primary key,
+  first_name varchar(50),
+  last_name varchar(50),
+  birth_date DATE
+);
+
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('Marc', 'Benichou', '1998-02-11');
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('Yoan', 'Cohen', '2010-12-03');
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('Lea', 'Benichou', '1987-07-27');
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('Amelia', 'Dux', '1996-04-07');
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('David', 'Grez', '2003-06-14');
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('Omer', 'Simpson', '1980-10-03');
+INSERT INTO students (first_name, last_name, birth_date) VALUES ('Maxime', 'Kajzer', '1996-06-17');
+
+
+SELECT * FROM students;
+SELECT first_name, last_name FROM students;
+SELECT first_name, last_name FROM students where id=2;
+SELECT first_name, last_name FROM students where last_name='Benichou' or first_name='Marc';
+SELECT first_name, last_name FROM students where first_name ~ '[a]';
+SELECT first_name, last_name FROM students where first_name ilike 'a%';
+SELECT first_name, last_name FROM students where first_name ilike '%a';
+SELECT first_name, last_name FROM students where position('a' in substring(first_name FROM 2))>0;
+SELECT first_name, last_name FROM students where id=1 or id=3;
+SELECT id, first_name, last_name, birth_date FROM students where birth_date>='1/01/2000';
+
+
