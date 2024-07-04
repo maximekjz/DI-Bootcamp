15409e12b9c4f4f9df3885e85f1a090b1a2735d5
maximekjz <maximekajzer@gmail.com>
2024-07-04 14:11:50 +0300

    XP

---
+++ Week5/Day5/XP.js
@@ -0,0 +1,20 @@
+const container_color = document.querySelectorAll('.container-colors > div:not(.button)');
+const mouseover_color = document.querySelectorAll('.main > div')
+
+container_color.forEach(div=>{
+    let color = div.style.backgroundColor;
+    div.addEventListener('click', function (){
+        console.log('Selected color:', color);
+        mouseover_color.forEach(div_over=>{
+            div_over.addEventListener('mouseover', function(){
+                this.style.backgroundColor = color;
+            })
+        })
+    });
+});
+
+function myMove(){
+    mouseover_color.forEach(div_over=>{
+    div_over.style.backgroundColor = 'white'
+})
+}
