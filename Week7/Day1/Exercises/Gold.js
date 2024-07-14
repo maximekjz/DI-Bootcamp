7cb7cc0010c5be8b718015a009b5b41b13ff2503
maximekjz <maximekajzer@gmail.com>
2024-07-14 15:02:52 +0300

    Gold

--- Week7/Day1/Exercises/Gold.js
+++ Week7/Day1/Exercises/Gold.js
@@ -6,15 +6,17 @@
 // Explain in a comment how Promise.all work and why you receive this output.
 
 
-    const promise1 = Promise.resolve(3);
-    const promise2 = 42;
-    const promise3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 3000, 'foo');
-    });
+    // const promise1 = Promise.resolve(3);
+    // const promise2 = 42;
+    // const promise3 = new Promise((resolve, reject) => {
+    // setTimeout(resolve, 3000, 'foo');
+    // });
 
-    Promise.all([promise1, promise2, promise3])
-        .then((results) => {console.log(results)})
-        .catch((error) => console.log(error))
+    // Promise.all([promise1, promise2, promise3])
+    //     .then((results) => {console.log(results)})
+    //     .catch((error) => console.log(error))
+
+
 // expected output: Array [3, 42, "foo"]
 
 
@@ -22,14 +24,17 @@
 // Instructions
 // Analyse the code below - what will be the output ?
 
-// function timesTwoAsync(x) {
-//   return new Promise(resolve => resolve(x * 2));
-// }
+function timesTwoAsync(x) {
+  return new Promise(resolve => resolve(x * 2));
+} 
+// ---> return 2 * x
 
-// const arr = [1, 2, 3];
-// const promiseArr = arr.map(timesTwoAsync);
+const arr = [1, 2, 3];
+const promiseArr = arr.map(timesTwoAsync); 
 
-// Promise.all(promiseArr)
-//   .then(result => {
-//     console.log(result);
-//   });
+Promise.all(promiseArr)
+  .then(result => {
+    console.log(result);
+  });
+
+//   --> [2,4,6]
