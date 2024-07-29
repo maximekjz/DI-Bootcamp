665787b402b271b1ddfc2a05c2c77703a899ed93
maximekjz <maximekajzer@gmail.com>
2024-07-29 19:44:08 +0300

    DC done

--- Week9/Day2/Exercises/DC/my-first-react/src/component/Vote.js
+++ Week9/Day2/Exercises/DC/my-first-react/src/component/Vote.js
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 function Vote() {
+  const style_container = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  }
+
   
   const style_language = {
-    display: 'inline-block',
-    border: '1px solid #000',
-    padding: '20px',
-    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    border: '1px solid #ddd',
+    padding: '10px',
+    width: '300px',
+    margin: '10px',
     backgroundColor: 'lightyellow',
   }
+
+
+  const style_button = {
+    alignSelf: 'flex-end',
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: 'green',
+    cursor: 'pointer',
+    marginTop: '10px',}
 
   const [languages, setLanguages] = useState([
     {name: "Php", votes: 0},
@@ -26,15 +47,17 @@ function Vote() {
 
   return(
     <>
-      <div style = {style_language}>
+      <div style = {style_container}>
         <h1>Vote your Language!</h1>
+        {languages.map((languages, index) =>(
+        <div key = {index} style= {style_language}>
+          <h5>{languages.votes}</h5>
+          <h5>                     </h5>
+          <h5>{languages.name}</h5>
+          <button style = {style_button} onClick={()=>addCount(index)}>Click Here!</button>
+        </div>
+        ))}
       </div>
-      {languages.map((languages, index) =>(
-      <div key = {index} style= {style_language}>
-        <h5>{languages.name}:{languages.votes}</h5>
-        <button onClick={()=>addCount(index)}>Click Here!</button>
-      </div>
-      ))}
     </>
   )
 
